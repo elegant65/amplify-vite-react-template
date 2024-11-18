3e6bc1378191c4e4f296c5a6891e08567b60f992
elegant65 <112301140+elegant65@users.noreply.github.com>
2024-11-18 19:47:05 +0530

    Update properties.ts

--- src/data/properties.ts
+++ src/data/properties.ts
@@ -5,20 +5,20 @@ export interface Property {
   price: string;
   description: string;
 }
-import villaImg from "../assets/villa.jpg";
+
 export const properties: Property[] = [
   {
     id: 1,
     name: "Luxury Villa in Malibu",
-    image: villaImg, //"/assets/villa.jpg",
-    price: "$3,000,000",
+    image: "../assets/villa.jpg",
+    price: "Rs 1 cr",
     description: "A stunning villa with ocean views and modern amenities.",
   },
   {
     id: 2,
     name: "Modern Apartment in New York",
-    image: "/assets/apartment.jpg",
-    price: "$1,500,000",
-    description: "A stylish apartment located in the heart of Manhattan.",
+    image: "../assets/apartment.jpg",
+    price: "Rs 2 cr",
+    description: "A stylish apartment located in the heart of Alwar.",
   },
 ];
