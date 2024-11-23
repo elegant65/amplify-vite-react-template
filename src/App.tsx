20cdbaf85abbc4382a1eb9f247c85a80a4c730fd
elegant65 <112301140+elegant65@users.noreply.github.com>
2024-11-23 18:19:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import project6Img from './assets/project6.jpg';
 import { useState } from 'react';
 import whatsappIcon from './assets/whatsapp-icon.png';
 
+import "swiper/swiper-bundle.min.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
